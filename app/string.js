5df7ad7095b98eb06d7fc5d962d770b28052ae8f
amirisback <faisalamircs@gmail.com>
2022-10-22 15:53:04 +0700

    update: readme bot

---
+++ app/string.js
@@ -0,0 +1,27 @@
+var text_app_linkedin   = "LinkedIn";
+var text_app_facebook   = "Facebook";
+var text_app_instagram  = "Instagram";
+var text_app_medium     = "Medium";
+var text_app_dribbble   = "Dribbble";
+var text_app_github     = "Github";
+
+var text_link_linkedin   = "linkedin.com/in/faisalamircs";
+var text_link_facebook   = "facebook.com/faisalamircs";
+var text_link_instagram  = "instagram.com/faisalamircs";
+var text_link_medium     = "medium.com/@faisalamircs";
+var text_link_dribbble   = "dribbble.com/faisalamircs";
+var text_link_github     = "github.com/amirisback";
+
+var quotes1 = "“The best way to predict the future is to invent it.” – Alan Kay";
+var quotes2 = "Fiqry Chowdhury";
+var quotes3 = "Amir";
+var quotes4 = "Cool";
+
+const quotesArray = [
+    quotes1,
+    quotes2,
+    quotes3,
+    quotes4
+];
+
+export default {quotesArray, text_link_github};
