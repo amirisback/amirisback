a96e17a35e55d72c14dcb0d4c91f24e9c1df8aa8
amirisback <faisalamircs@gmail.com>
2022-10-28 21:58:14 +0700

    add: endpoint.js

--- app/endpoint.js
+++ app/endpoint.js
@@ -1,18 +1,27 @@
+// const fs = require('fs');
 const server1 = "https://amirisback.github.io/"; // server1
 const server2 = "https://xeonidas-xcreator.github.io/"; // server2
 
-const data = [
-    {
-        "base_url": server1,
-        "social_media": "",
-        "link": ""
-    },
-    {
-        "base_url": server2,
-        "social_media": "",
-        "link": "" 
-    }      
-]
+class Endpoind {
+  base_url; 
+  social_media; 
+  link;
+  
+  constructor(base_url, social_media, link) {
+    this.base_url = base_url;
+    this.social_media = social_media;
+    this.link = link;
+  }
 
-const rand = Math.floor(Math.random() * data.length)
-console.log(data[rand])
+}
+
+const endpoint1 = new Endpoind(server1, "Github", "");
+const endpoint2 = new Endpoind(server2, "Github", "");
+
+const data = [endpoint1, endpoint2];
+const json = JSON.stringify(data);
+
+// fs.writeFileSync('data/baru.json', json, 'utf8');
+// const rand = Math.floor(Math.random() * data.length)
+
+console.log(json)
