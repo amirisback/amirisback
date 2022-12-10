e3ec0947575623331b9be9abe887a3a1bc5875b5
amirisback <faisalamircs@gmail.com>
2022-12-10 21:20:44 +0700

    update: README.js

--- app/readme.js
+++ app/readme.js
@@ -4,12 +4,65 @@ function getReadme() {
     const quotes2 = "“There are only two hard things in Computer Science: cache invalidation and naming things.” – Phil Karlton";
     const quotes3 = "“I Can Do Whatever I Want Today“";
     const quotes4 = "“Object-oriented programming languages support encapsulation, thereby improving the ability of software to be reused, refined, tested, maintained, and extended. The full benefit of this support can only be realized if encapsulation is maximized during the design process.“ - Rebecca Wirfs-Brock";
+    const quotes5 = "“If it hurts, do it more frequently, and bring the pain forward.” ― Jez Humble";
+    const quotes6 = "“You must excuse my gruff conduct,” the watchdog said, after they’d been driving for some time, “but you see it’s traditional for watchdogs to be ferocious.” ― Robert C. Martin, Clean Architecture ";
+    const quotes7 = "“Engineering is a profession that can do the job of almost all other professions.” ― Amit Kalantri, Wealth of Words";
+    const quotes8 = "“People are inherently imperfect - we like to say that humans are mostly a collection of intermittent bugs. But before you can understand the bugs in your coworkers, you need to understand the bugs in yourself. We’re going to ask you to think about your own reactions, behaviors, and attitudes - and in return, we hope you gain some real insight into how to become a more efficient and successful software engineer who spends less energy dealing with people-related problems and more time writing great code.” ― Titus Winters,";
+    const quotes9 = "“Software and cathedrals are much the same; first we build them, then we pray.” by Anonymous.";
+    const quotes10 = "“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” by Martin Fowler.";
+    const quotes11 = "“Real programmers can write assembly code in any language.” by Larry Wall.";
+    const quotes12 = "“Good programmers use their brains, but good guidelines save us having to think out every case.” by Francis Glassborow.";
+    const quotes13 = "“Computer programs are the most complex things that humans make” by Douglas Crockford.";
+    const quotes14 = "“A language that doesn't affect the way you think about programming is not worth knowing” by Alan Perlis.";
+    const quotes15 = "“A language that doesn't have everything is actually easier to program in than some that do.” by Dennis Ritchie.";
+    const quotes16 = "“9 women cannot make a baby in one month.” by Fred Brooks.";
+    const quotes17 = "“Adding manpower to a late software project makes it later.” by Fred Brook";
+    const quotes18 = "“Adding last-minute features, whether in response to competitive pressure, as a developer’s pet feature, or on the whim of management, causes more bugs in software than almost anything else.” by John Robbins.";
+    const quotes19 = "“Smart data structures and dumb code works a lot better than the other way around.” by Eric S Raymond.";
+    const quotes20 = "“Design and programming are human activities; forget that and all is lost” by Bjarne Stroustrup.";
+    const quotes21 = "“The wages of sin is debugging” by Ron Jeffries.";
+    const quotes22 = "“If debugging is the process of removing software bugs, then programming must be the process of putting them in” by Edsger Dijkstra.";
+    const quotes23 = "“To be a programmer is to develop a carefully managed relationship with error. There’s no getting around it. You either make your accommodations with failure, or the work will become intolerable.” by Ellen Ullman.";
+    const quotes24 = "“Any program is only as good as it is useful.” by Linus Torvalds.";
+    const quotes25 = "“Time pressure gradually corrupts an engineer’s standard of quality and perfection. It has a detrimental effect on people as well as products.” by Niklaus Wirth.";
+    const quotes26 = "“Software is like sex. It’s better when it’s free.” by Linus Torvalds.";
+    const quotes27 = "“The function of good software is to make the complex appear to be simple” by Grady Booch.";
+    const quotes28 = "“Code should run as fast as necessary, but no faster; something important is always traded away to increase speed.” by Richard E. Pattis.";
+    const quotes29 = "“The biggest issue on software teams is making sure everyone understands what everyone else is doing.” by Martin Fowler.";
+    const quotes30 = "“Poor management can increase software costs more rapidly than any other factor.” by Barry Boehm.";
+
 
     const quotesArray = [
         quotes1,
         quotes2,
         quotes3,
-        quotes4
+        quotes4,
+        quotes5,
+        quotes6,
+        quotes7,
+        quotes8,
+        quotes9,
+        quotes10,
+        quotes11,
+        quotes12,
+        quotes13,
+        quotes14,
+        quotes15,
+        quotes16,
+        quotes17,
+        quotes18,
+        quotes19,
+        quotes20,
+        quotes21,
+        quotes22,
+        quotes23,
+        quotes24,
+        quotes25,
+        quotes26,
+        quotes27,
+        quotes28,
+        quotes29,
+        quotes30
     ];
     
     const idx = Math.floor(Math.random() * quotesArray.length)
