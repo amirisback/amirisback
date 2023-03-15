ac1ca897c7623b46070fc94329996789494167da
Faisal Amir <faisalamircs@gmail.com>
2023-03-15 09:27:57 +0700

    update: Readme js

--- app/readme.js
+++ app/readme.js
@@ -65,7 +65,7 @@ function getReadme() {
         quotes30
     ];
     
-    const idx = Math.floor(Math.random() * quotesArray.length)
+    // const idx = Math.floor(Math.random() * quotesArray.length)
 
     const imageA1 = '<img id="imageB" width="110px" height="120px" align="right" src="https://amirisback.github.io/amirisback/docs/image/bear-panda/bear-funny-dancing.gif">';
     const imageA2 = '<img id="imageB" width="110px" height="120px" align="right" src="https://amirisback.github.io/amirisback/docs/image/bear-panda/bear-funny-dancing-02.gif">';
@@ -109,36 +109,56 @@ function getReadme() {
 
     const idxImage = Math.floor(Math.random() * imagesA.length)
 
+
+//     return `\
+// # 👋🏻 Welcome To Amir's Open Source Project
+// [![Medium Badge](https://img.shields.io/badge/-faisalamircs-black?style=flat-square&logo=Medium&logoColor=white&link=https://medium.com/@faisalamircs)](https://medium.com/@faisalamircs)
+// [![Android Arsenal](https://img.shields.io/badge/Android%20Arsenal-amirisback-brightgreen.svg?style=flat-square)](https://android-arsenal.com/user/amirisback)
+// [![Google Badge](https://img.shields.io/badge/Google%20Dev%20Library-amirisback-orange?style=flat-square)](https://devlibrary.withgoogle.com/authors/amirisback)
+
+// #### What are Amir doing? Amir crafting some cool apps and libraries 🔨
+
+// ${imagesB[idxImage]}
+// ${imagesA[idxImage]}
+
+// - Amir's Awesome Project and Libraries - [Click Here](https://github.com/amirisback/awesome-project-catalogue)
+// - Kotlin Desktop Experimental - [Click Here](https://github.com/amirisback/desktop-experimental-catalgoue)
+// - Android Code Playground, Android Research - [Click Here](https://github.com/armorycodes)
+// - Android Programmer Interview Cheat Sheet - [Click Here](https://amirisback.github.io/android-programmer-interview-cheat-sheet/)
+
+// #### Project Code Sample Implementation
+// - Minimum MVVM ([Koin](https://github.com/InsertKoinIO/koin)) Sample - [Simple](https://github.com/amirisback/minimum-mvvm-koin), [HTTP Client](https://github.com/amirisback/minimum-mvvm-koin-network-api), [Room Database](https://github.com/amirisback/minimum-mvvm-koin-room-database)
+// - Minimum MVVM ([Dagger-Hilt](https://dagger.dev/hilt/gradle-setup)) Sample - [Simple](https://github.com/amirisback/minimum-mvvm-hilt), [HTTP Client](https://github.com/amirisback/minimum-mvvm-hilt-network-api), [Room Database](https://github.com/amirisback/minimum-mvvm-hilt-room-database)
+// - Jitpack Library Publishing Guide - [Groovy](https://github.com/amirisback/jitpack-library-groovy-guide), [Kotlin DSL](https://github.com/amirisback/jitpack-library-kotlin-dsl-guide), [Kotlin Library](https://github.com/amirisback/easy-kotlin-lib-jar) 
+// - Automated Build Android AAB / APK Using Github Action - [Click Here](https://github.com/amirisback/automated-build-android-app-with-github-action)
+
+// <!-- #### Support Me 💖 :rocket: -->
+// #### Support Me :rocket:
+// - Wall of Fame Angel Sponsor - [Click Here](https://amirisback.github.io/wall-of-fame-angel-sponsor/)
+// - Sponsors Pages - [Click Here](https://github.com/sponsors/amirisback)
+// - Project Open Sources - [Click Here](https://github.com/users/amirisback/projects/17)
+
+// #### Quotes For Today
+// > ${quotesArray[idx]}
+
+// `
+
+
     return `\
 # 👋🏻 Welcome To Amir's Open Source Project
 [![Medium Badge](https://img.shields.io/badge/-faisalamircs-black?style=flat-square&logo=Medium&logoColor=white&link=https://medium.com/@faisalamircs)](https://medium.com/@faisalamircs)
 [![Android Arsenal](https://img.shields.io/badge/Android%20Arsenal-amirisback-brightgreen.svg?style=flat-square)](https://android-arsenal.com/user/amirisback)
 [![Google Badge](https://img.shields.io/badge/Google%20Dev%20Library-amirisback-orange?style=flat-square)](https://devlibrary.withgoogle.com/authors/amirisback)
 
-#### What are Amir doing? Amir crafting some cool apps and libraries 🔨
+<!-- #### Support Me 💖 :rocket: -->
+#### Support Me :rocket:
 
 ${imagesB[idxImage]}
 ${imagesA[idxImage]}
 
-- Amir's Awesome Project and Libraries - [Click Here](https://github.com/amirisback/awesome-project-catalogue)
-- Kotlin Desktop Experimental - [Click Here](https://github.com/amirisback/desktop-experimental-catalgoue)
-- Android Code Playground, Android Research - [Click Here](https://github.com/armorycodes)
-- Android Programmer Interview Cheat Sheet - [Click Here](https://amirisback.github.io/android-programmer-interview-cheat-sheet/)
-
-#### Project Code Sample Implementation
-- Minimum MVVM ([Koin](https://github.com/InsertKoinIO/koin)) Sample - [Simple](https://github.com/amirisback/minimum-mvvm-koin), [HTTP Client](https://github.com/amirisback/minimum-mvvm-koin-network-api), [Room Database](https://github.com/amirisback/minimum-mvvm-koin-room-database)
-- Minimum MVVM ([Dagger-Hilt](https://dagger.dev/hilt/gradle-setup)) Sample - [Simple](https://github.com/amirisback/minimum-mvvm-hilt), [HTTP Client](https://github.com/amirisback/minimum-mvvm-hilt-network-api), [Room Database](https://github.com/amirisback/minimum-mvvm-hilt-room-database)
-- Jitpack Library Publishing Guide - [Groovy](https://github.com/amirisback/jitpack-library-groovy-guide), [Kotlin DSL](https://github.com/amirisback/jitpack-library-kotlin-dsl-guide), [Kotlin Library](https://github.com/amirisback/easy-kotlin-lib-jar) 
-- Automated Build Android AAB / APK Using Github Action - [Click Here](https://github.com/amirisback/automated-build-android-app-with-github-action)
-
-<!-- #### Support Me 💖 :rocket: -->
-#### Support Me :rocket:
 - Wall of Fame Angel Sponsor - [Click Here](https://amirisback.github.io/wall-of-fame-angel-sponsor/)
 - Sponsors Pages - [Click Here](https://github.com/sponsors/amirisback)
 - Project Open Sources - [Click Here](https://github.com/users/amirisback/projects/17)
-
-#### Quotes For Today
-> ${quotesArray[idx]}
 
 `
 }
